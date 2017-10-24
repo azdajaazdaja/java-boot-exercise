6f8e9926fdd935a4d7221f4d7faf6aeebc24d1eb
Azdaja <azdajaazdaja@gmail.com>
2017-10-24 15:20:44 +0200

    Added Integration test for the POST method SensorResource::addMeasurement

--- src/test/java/com/exercise/rest/SensorResourceIntegrationTest.java
+++ src/test/java/com/exercise/rest/SensorResourceIntegrationTest.java
@@ -1,6 +1,15 @@
 package com.exercise.rest;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasToString;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+
+import java.sql.Timestamp;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,12 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.hasItems;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpStatus.OK;
+import io.restassured.RestAssured;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -56,4 +60,25 @@ public class SensorResourceIntegrationTest {
                 body("sensorId", hasItems("sensor-teststreet-1"));
     }
 
+    @Test
+    public void should_add_Measurement() {
+    	// add measurement
+    	given().
+    			contentType(JSON).
+    			body("{ \"sensorId\": \"1\", \"value\": \"55.7\", \"timestamp\": \"2017-09-18\" }").
+    	when().
+    			post("/sensors/measurement").
+    	then().
+    			statusCode(OK.value());
+    	
+    	// get measurement
+        when().
+        		get("/sensors/measurements").
+        then().
+        		statusCode(OK.value()).
+        		body("sensorId", hasItems(hasToString("1"))).
+        		body("value", hasItems(hasToString("55.7"))).
+        		body("timestamp", hasItems(hasToString(Long.toString(Timestamp.valueOf("2017-09-18 02:00:00.000").getTime()))));	
+    }
+    
 }
